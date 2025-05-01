5e03ab3472a1e0d7fd361e0f3fe3cf6b8d372166
Jared Adlawan <adlaw1.jareeed@gmail.com>
2025-05-01 16:44:04 +0800

    adjusted rental analytics

---
+++ Controls/TotRentControl.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace ArcadeSync_Project.Controls
+{
+    partial class TotRentControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // TotRentControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "TotRentControl";
+            Size = new Size(1080, 665);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
